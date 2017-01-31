24284110b936f344555bafbc596f2dbe1d299a8b
Andrii Savchuk <andriksr@gmail.com>
2017-01-31 12:34:41 +0200

    SoftServe Test Task

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Andy on 25.01.2017.
+ */
+
+$(document).ready(function(){
+
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
+
+});
